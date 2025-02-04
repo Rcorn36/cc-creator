1003cd50594b68aa8105ccf9008b5ab9cb35ce6a
Rob Cornett <rcorn36@wgu.edu>
2025-02-03 19:22:25 -0800

    fixsecureerror

--- data/env/server.ts
+++ data/env/server.ts
@@ -1,5 +1,8 @@
 import { createEnv } from "@t3-oss/env-nextjs"
 import { z } from "zod"
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const env = createEnv({
     emptyStringAsUndefined: true,
@@ -9,4 +12,4 @@ export const env = createEnv({
         CLERK_WEBHOOK_SECRET: z.string(),
       },
       experimental__runtimeEnv: process.env,
-    })
+    });
