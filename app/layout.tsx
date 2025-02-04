3520082c313bb3a25ac45da6178721aadaad61de
Rob Cornett <rcorn36@wgu.edu>
2025-02-03 18:04:51 -0800

    yeehaw

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
+  ClerkProvider
+  
 } from '@clerk/nextjs'
 
 const geistSans = Geist({
